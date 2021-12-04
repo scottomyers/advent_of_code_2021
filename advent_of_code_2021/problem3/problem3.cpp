ad4f0159f1d1ab733db16411a3755c0e8f0fb41c
scottomyers <6031789+scottomyers@users.noreply.github.com>
2021-12-03 21:41:00 -0600

    problem 3 part 2

--- advent_of_code_2021/problem3/problem3.cpp
+++ advent_of_code_2021/problem3/problem3.cpp
@@ -6,6 +6,17 @@
 
 constexpr int bitset_size{ 12 };
 
+std::vector<std::string> read_lines(const std::string filename)
+{
+	std::string line{};
+	std::vector<std::string> lines{};
+	std::ifstream infile{ filename };
+	while (std::getline(infile, line)) {
+		lines.push_back(line);
+	}
+	return lines;
+}
+
 std::bitset<bitset_size> calculate_gamma(const std::vector<int>& zeroes, const std::vector<int>& ones)
 {
 	std::bitset<bitset_size> gamma{};
@@ -33,14 +44,12 @@ std::bitset<bitset_size> calculate_epsilon(std::bitset<bitset_size>& gamma, int 
 	return epsilon & mask;
 }
 
-void solve_part1(std::istream& is)
+void solve_part1(const std::vector<std::string>& lines)
 {
-	std::string line{};
-	std::getline(is, line);
-	std::vector<int> zeroes(line.length(), 0);
-	std::vector<int> ones(line.length(), 0);
-	
-	while (std::getline(is, line)) {
+	std::vector<int> zeroes(lines[0].length(), 0);
+	std::vector<int> ones(lines[0].length(), 0);
+
+	for (auto& line : lines) {
 		for (auto i{ 0 }; i < line.length(); ++i) {
 			auto c = line[i];
 			if (c == '0') {
@@ -54,15 +63,71 @@ void solve_part1(std::istream& is)
 
 	std::bitset<bitset_size> gamma{ calculate_gamma(zeroes, ones) };
 	std::bitset<bitset_size> epsilon{ calculate_epsilon(gamma, static_cast<int>(zeroes.size())) };
-	std::cout << "Answer: " << (gamma.to_ulong() * epsilon.to_ulong()) << std::endl;
+	std::cout << "Part 1: " << (gamma.to_ulong() * epsilon.to_ulong()) << std::endl;
+}
+
+unsigned long long calculate_oxygen_rating(const std::vector<std::string>& lines, int position = 0)
+{
+	if (lines.size() == 1) {
+		return std::bitset<12>{lines[0]}.to_ullong();
+	}
+
+	int zeroes{ 0 };
+	int ones{ 0 };
+	for (auto& line : lines) {
+		if (line[position] == '1') ++ones;
+		else if (line[position] == '0') ++zeroes;
+	}
+
+	char winner{ '0' };
+	if (ones >= zeroes) winner = '1';
+
+	std::vector<std::string> candidates{};
+	for (auto line : lines) {
+		if (line[position] == winner) candidates.push_back(line);
+	}
+	return calculate_oxygen_rating(candidates, ++position);
+}
+
+unsigned long long calculate_co2_rating(const std::vector<std::string>& lines, int position = 0)
+{
+	if (lines.size() == 1) {
+		return std::bitset<12>{lines[0]}.to_ullong();
+	}
+
+	int zeroes{ 0 };
+	int ones{ 0 };
+	for (auto& line : lines) {
+		if (line[position] == '1') ++ones;
+		else if (line[position] == '0') ++zeroes;
+	}
+
+	char winner{ '0' };
+	if (ones < zeroes) winner = '1';
+
+	std::vector<std::string> candidates{};
+	for (auto line : lines) {
+		if (line[position] == winner) candidates.push_back(line);
+	}
+	return calculate_co2_rating(candidates, ++position);
+}
+
+void solve_part2(const std::vector<std::string>& lines)
+{
+	unsigned long long co2{ calculate_co2_rating(lines) };
+	unsigned long long o2{ calculate_oxygen_rating(lines) };
+	std::cout << "Part 2: " << (o2 * co2) << std::endl;
 }
 
 int main()
 {
-	std::ifstream example{ "example-input.txt" };
-	solve_part1(example);
+	std::vector<std::string> example_input{ read_lines("example-input.txt") };
+	solve_part1(example_input);
 
-	std::ifstream input{ "input.txt" };
+	std::vector<std::string> input{ read_lines("input.txt") };
 	solve_part1(input);
+
+	solve_part2(example_input);
+	solve_part2(input);
 	return 0;
 }
