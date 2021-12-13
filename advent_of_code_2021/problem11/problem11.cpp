56440debfe0e0d0690eb324ba2073fea9f42320d
scottomyers <6031789+scottomyers@users.noreply.github.com>
2021-12-12 22:04:05 -0600

    add visulization for problem 11

--- advent_of_code_2021/problem11/problem11.cpp
+++ advent_of_code_2021/problem11/problem11.cpp
@@ -1,5 +1,42 @@
 #include "octopus.h"
 #include <iostream>
+#include <Windows.h>
+
+void cls(HANDLE hConsole)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	SMALL_RECT scrollRect;
+	COORD scrollTarget;
+	CHAR_INFO fill;
+
+	// Get the number of character cells in the current buffer.
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+		return;
+	}
+
+	// Scroll the rectangle of the entire buffer.
+	scrollRect.Left = 0;
+	scrollRect.Top = 0;
+	scrollRect.Right = csbi.dwSize.X;
+	scrollRect.Bottom = csbi.dwSize.Y;
+
+	// Scroll it upwards off the top of the buffer with a magnitude of the entire height.
+	scrollTarget.X = 0;
+	scrollTarget.Y = (SHORT)(0 - csbi.dwSize.Y);
+
+	// Fill with empty spaces with the buffer's default text attribute.
+	fill.Char.UnicodeChar = TEXT(' ');
+	fill.Attributes = csbi.wAttributes;
+
+	// Do the scroll
+	ScrollConsoleScreenBuffer(hConsole, &scrollRect, NULL, scrollTarget, &fill);
+
+	// Move the cursor to the top left corner too.
+	csbi.dwCursorPosition.X = 0;
+	csbi.dwCursorPosition.Y = 0;
+
+	SetConsoleCursorPosition(hConsole, csbi.dwCursorPosition);
+}
 
 std::vector<int> example_data{
 	5,4,8,3,1,4,3,2,2,3,
@@ -75,7 +112,29 @@ void part2()
 
 int main()
 {
-	part1();
-	part2();
+	part1(); // 1637
+	part2(); // 242
+
+	Sleep(3000);
+
+	// visualization -- Windows only 
+	HANDLE hStdout;
+
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	cls(hStdout);
+
+	p11::OctopusGrid og{ my_data };
+	int cycles{ 0 };
+	while (true) {
+		og.print();
+		std::cout << "Flashes: " << og.current_flashes() << '\n';
+		std::cout << "Total flashes: " << og.total_flashes() << '\n';
+		std::cout << "Total cycles: " << cycles << std::endl;
+		og.step();
+		++cycles;
+		Sleep(1000);
+		cls(hStdout);
+	}
 	return 0;
 }
